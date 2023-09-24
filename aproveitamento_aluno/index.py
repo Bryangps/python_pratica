c2dabe02ce15d3eb331690491378c7d0bd126eaa
Bryan Silva <bryan.gps18@gmail.com>
2023-09-24 14:48:44 -0300

    trocando a estrutura de condição

--- aproveitamento_aluno/index.py
+++ aproveitamento_aluno/index.py
@@ -10,19 +10,15 @@ print('-' * 20)
 print('MEDIA: {}'.format(media))
 if media >= 9:
     print('APROVEITAMENTO A')
+elif (media >= 8) and (media < 9):
+    print('APROVEITAMENTO B')
+elif (media >= 7) and (media < 8):
+    print('APROVEITAMENTO C')
+elif (media >= 6) and (media < 7):
+    print('APROVEITAMENTO D')
+elif (media >= 5) and (media < 6):
+    print('APROVEITAMENTO E')
 else:
-    if (media >= 8) and (media < 9):
-        print('APROVEITAMENTO B')
-    else:
-        if (media >= 7) and (media < 8):
-            print('APROVEITAMENTO C')
-        else:
-            if (media >= 6) and (media < 7):
-                print('APROVEITAMENTO D')
-            else:
-                if (media >= 5) and (media < 6):
-                    print('APROVEITAMENTO E')
-                else:
-                    print('APROVEITAMENTO F')
+    print('APROVEITAMENTO F')
 
 print('_' * 20)
